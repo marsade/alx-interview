77408f7be6bf57111e0eeead0592a0f833b27fe6
Mars Ade <adenijifara@gmail.com>
2024-10-06 21:37:34 +0300

    Move file

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+
+def pascal_triangle(n):
+    ml = []
+    if n <= 0:
+        return []
+    else:
+        for i in range(1, n+1):
+            pt = []
+            C = 1
+            for j in range(1, i+1):
+                pt.append(C)
+                C = C * (i -  j) // j
+            ml.append(pt)
+        return ml
---
+++ 0x00-pascal_triangle/README.md
@@ -0,0 +1,2 @@
+# 0-pascal_triangle.py
+This directory contains the file for the implementation of pascal's triangle with python
