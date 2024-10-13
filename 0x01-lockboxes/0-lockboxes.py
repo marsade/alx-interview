a04a4ec733ac61bee8546bf14a3012e95890ee78
mars <adenijifara@gmail.com>
2024-10-13 21:42:50 +0300

    Complete

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+'''This script solves the Lockbox problem'''
+
+def canUnlockAll(boxes):
+    """ Method that determines if all boxes can be opened """
+
+    for key in range(1, len(boxes)):
+        flag = False
+        for box in range(len(boxes)):
+            if key in boxes[box] and box != key:
+                flag = True
+                break
+        if not flag:
+            return False
+
+    return True
