95e86e64e5fd4ce4b24435a0c0485800bead5a87
mars <adenijifara@gmail.com>
2024-11-21 15:27:07 +0200

    Task complete

---
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+'''Rotate a 2D matrix'''
+
+def rotate_2d_matrix(matrix):
+    '''Rotates a 2d matrix in place
+    
+    Args: (matrix) - matrix to be rotated
+    Returns: (None) - matrix is rotated in place
+    '''
+
+    l = len(matrix)
+
+    res = [[0] * l for _ in range(l)]
+
+    for i in range(l):
+        for j in range(l):
+            res[j][l-1-i] = matrix[i][j]
+
+    for i in range(l):
+        for j in range(l):
+            matrix[i][j] = res[i][j]
